5cabba02ca7fc3e5e555c1ab959c5a6ab67368ab
truckcarr11 <tylercarr1111@yahoo.com>
2023-08-07 10:42:58 -0500

    Added more styling and state management

--- MultiSelect.js
+++ MultiSelect.js
@@ -1,39 +1,96 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function MultiSelect(props) {
   const [expanded, setExpanded] = useState(true);
-  const [selectedItems, setSelectedItems] = useState([]);
-  const [selectableItems, setSelectableItems] = useState(props.options);
+  const [selectedOptions, setSelectedOptions] = useState([]);
+  const [selectableOptions, setSelectableOptions] = useState(props.options);
+
+  function handleOptionClick(clickedOption) {
+    setSelectedOptions((currentSelectedOptions) =>
+      !currentSelectedOptions.includes(clickedOption)
+        ? [...selectedOptions, clickedOption]
+        : currentSelectedOptions.filter(
+            (option) => option.id !== clickedOption.id
+          )
+    );
+  }
+
+  useEffect(() => {
+    console.log(selectedOptions);
+  }, [selectedOptions]);
 
   return (
     <div>
       <div
-        className="border rounded-lg flex justify-between p-2"
+        tabindex="0"
+        className="border rounded-lg flex justify-between p-2 cursor-pointer focus:ring focus:ring-blue-500 focus:outline-none"
         onClick={() => setExpanded((currentExpanded) => !currentExpanded)}
       >
-        <div>{props.placeholder}</div>
-        <div>V</div>
+        <div>{selectedOptions.length > 0 ? 'Options' : props.placeholder}</div>
+        {expanded ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-5 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M4.5 15.75l7.5-7.5 7.5 7.5"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-5 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+            />
+          </svg>
+        )}
       </div>
       {expanded && (
-        <ol className="border mt-1 rounded-lg">
-          {props.options.slice(0, 1).map((item) => (
-            <li key={item.id} className="pl-2 hover:bg-gray-300 rounded-t-lg">
-              {item.value}
+        <ul className="border mt-1 rounded-lg shadow-md cursor-pointer">
+          {props.options.slice(0, 1).map((option) => (
+            <li
+              key={option.id}
+              className="pl-2 hover:bg-gray-300 rounded-t-lg"
+              onClick={() => handleOptionClick(option)}
+            >
+              {option.value}
             </li>
           ))}
-          {props.options.slice(1, props.options.length - 1).map((item) => (
-            <li key={item.id} className="pl-2 hover:bg-gray-300">
-              {item.value}
+          {props.options.slice(1, props.options.length - 1).map((option) => (
+            <li
+              key={option.id}
+              className="pl-2 hover:bg-gray-300"
+              onClick={() => handleOptionClick(option)}
+            >
+              {option.value}
             </li>
           ))}
           {props.options
             .slice(props.options.length - 1, props.options.length)
-            .map((item) => (
-              <li key={item.id} className="pl-2 hover:bg-gray-300 rounded-b-lg">
-                {item.value}
+            .map((option) => (
+              <li
+                key={option.id}
+                className="pl-2 hover:bg-gray-300 rounded-b-lg"
+                onClick={() => handleOptionClick(option)}
+              >
+                {option.value}
               </li>
             ))}
-        </ol>
+        </ul>
       )}
     </div>
   );
